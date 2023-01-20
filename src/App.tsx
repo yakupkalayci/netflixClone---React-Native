41fd22c4b524847f15846dcb7bcb222a5f8a22b2
yakupkalayci <ykpklyc18@gmail.com>
2023-01-20 12:22:44 +0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+import Welcome from './screens/Welcome';
+
+const Stack = createNativeStackNavigator();
+
+function App(): JSX.Element {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name="Home" component={Welcome} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
